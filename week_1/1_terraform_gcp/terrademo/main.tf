7a6821c6e5303cb9c7b092e24ee3d6c64383c83e
joeWatersDev <joesegerdev@gmail.com>
2025-02-02 19:41:31 -0500

    Implement variables for our terraform file

--- week_1/1_terraform_gcp/terrademo/main.tf
+++ week_1/1_terraform_gcp/terrademo/main.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 provider "google" {
-  credentials = "./keys/my-creds.json"
-  project = "terraform-demo-449422"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "terraform-demo-449422-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -27,4 +27,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+
 }
